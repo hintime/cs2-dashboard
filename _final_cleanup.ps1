1544ca20a9baa97d89cdd88ad7cf2e72c3156f27
hintime <hintime@users.noreply.github.com>
2026-04-21 01:49:11 +0800

    chore: remove remaining debug files

---
+++ _final_cleanup.ps1
@@ -0,0 +1,5 @@
+﻿Set-Location 'C:\Users\Lenovo\.qclaw\workspace\cs2-dashboard'
+Remove-Item _cleanup.ps1, _verify.py, _verify.ps1, _verify_result.txt -ErrorAction SilentlyContinue
+git add -A 2>&1
+git commit -m "chore: remove remaining debug files" 2>&1
+git push 2>&1
